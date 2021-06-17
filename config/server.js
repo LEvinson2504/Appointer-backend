dbc689574b984d060d2c5b15aa3ee1c7717815da
LEvinson2504 <ldsouza2504@gmail.com>
2021-06-17 20:28:19 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b5d0ab10221f7408ded957a71040015'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3ce0f10d-9132-485c-b863-cd8ef561ca6b'
+};
